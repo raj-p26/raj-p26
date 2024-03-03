535f87e4cab8a8757067677e75c68a05223412b7
raj-p26 <rajpeshavariya26@gmail.com>
2024-03-03 22:47:31 +0530

    immediate commit

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -8,18 +8,10 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
 
   use({
       "folke/trouble.nvim",
@@ -70,9 +62,9 @@ return require('packer').startup(function(use)
   }
 
   use("folke/zen-mode.nvim")
-  use("github/copilot.vim")
   use("eandrju/cellular-automaton.nvim")
   use("laytan/cloak.nvim")
-
+  use("projekt0n/github-nvim-theme")
+  use "Exafunction/codeium.vim"
 end)
 
